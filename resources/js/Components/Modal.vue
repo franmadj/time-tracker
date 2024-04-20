86ec917d05ecb3048e099a60579c7e5ad24a25ab
Francisco Mauri <labrest03@gmail.com>
2024-04-21 01:18:01 +0200

    widther modal

--- resources/js/Components/Modal.vue
+++ resources/js/Components/Modal.vue
@@ -55,6 +55,7 @@ const maxWidthClass = computed(() => {
         lg: 'sm:max-w-lg',
         xl: 'sm:max-w-xl',
         '2xl': 'sm:max-w-2xl',
+        'screenlg':'sm:max-w-screen-lg'
     }[props.maxWidth];
 });
 </script>
