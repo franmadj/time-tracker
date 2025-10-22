f9a03f8e88c46fab23a9071bfc8069f9b0718c27
Francisco Mauri <labrest03@gmail.com>
2025-10-23 00:53:34 +0200

    chore: rename Home navigation link to Clients in authenticated layout

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -30,7 +30,7 @@ const showingNavigationDropdown = ref(false);
                             <!-- Navigation Links -->
                             <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                                 <NavLink :href="route('client.index')" :active="route().current('client.index')">
-                                    Home
+                                    Clients
                                 </NavLink>
                                 <NavLink :href="route('shortcuts.index')" :active="route().current('shortcuts.index')">
                                     Shortcuts
